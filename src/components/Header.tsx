6a62e624bb189527d27830d487524b435237bc0c
kainamf <kaina.marques@calindra.com.br>
2025-07-26 19:32:52 -0300

    feat: simplify Header component styles by removing unnecessary color classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,15 @@ function Header({ scrollToSection }: { scrollToSection: (sectionId: string) => v
         <header className="bg-secondary-500 text-white py-4 shadow-md">
             <div className="container mx-auto px-6 py-4">
                 <div className="flex justify-between items-center">
-                    <div className="text-3xl font-bold text-primary-700 text-font">
+                    <div className="text-3xl font-bold text-font">
                         Lara Frasson
-                        <div className="text-base font-normal text-primary-900 text-font">Psicóloga Infanto Juvenil CRP {CRP}</div>
+                        <div className="text-base font-normal text-font">Psicóloga Infanto Juvenil CRP {CRP}</div>
                     </div>
                     <nav className="hidden md:flex space-x-8">
-                        <button onClick={() => scrollToSection('inicio')} className="text-lg text-primary-700 hover:text-primary-900 text-font transition-colors">Início</button>
-                        <button onClick={() => scrollToSection('servicos')} className="text-lg text-primary-700 hover:text-primary-900 text-font transition-colors">Serviços</button>
-                        <button onClick={() => scrollToSection('sobre')} className="text-lg text-primary-700 hover:text-primary-900 text-font transition-colors">Sobre</button>
-                        <button onClick={() => scrollToSection('contato')} className="text-lg text-primary-700 hover:text-primary-900 text-font transition-colors">Contato</button>
+                        <button onClick={() => scrollToSection('inicio')} className="text-lg text-font transition-colors">Início</button>
+                        <button onClick={() => scrollToSection('servicos')} className="text-lg text-font transition-colors">Serviços</button>
+                        <button onClick={() => scrollToSection('sobre')} className="text-lg text-font transition-colors">Sobre</button>
+                        <button onClick={() => scrollToSection('contato')} className="text-lg text-font transition-colors">Contato</button>
                     </nav>
                 </div>
             </div>
