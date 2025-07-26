68f35e70d8f9c5159836e60721f97fc8a7fc63b9
kainamf <kaina.marques@calindra.com.br>
2025-07-26 19:46:42 -0300

    feat: remove Header component to streamline layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
@@ -8,14 +6,8 @@ import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
 
 function App() {
-    const scrollToSection = (sectionId: string) => {
-        const element = document.getElementById(sectionId);
-        element?.scrollIntoView({ behavior: 'smooth' });
-    };
-
     return (
         <div className="min-h-screen bg-white">
-            <Header scrollToSection={scrollToSection} />
             <Hero />
             <Services />
             <About />
