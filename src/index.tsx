2ee433751f4720a16f55b27db9a2889452251a5a
kainamf <kaina.marques@calindra.com.br>
2025-10-22 21:58:59 -0300

    refactor: remove react-helmet-async and related code from Empatea component and delete FAQ component

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './views/App';
-import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </React.StrictMode>
 );
